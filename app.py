64b12af90d8feaaf91c1704c89aedd63dcb884e3
Aarsha <aarshakraj9@gmail.com>
2026-07-03 10:20:19 +0530

    Remove duplicate title in Streamlit app

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 st.title("take the input")
 name= st.text_input("enter the name")
-st.title("take the input")
 if st.button("submit"):
   st.write(f"print the name: {name}")
   
